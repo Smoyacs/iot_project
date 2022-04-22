dcb8eaec35334c5e4b5ca9e463096575ca2ce9c8
Smoyacs <95979752+Smoyacs@users.noreply.github.com>
2022-04-22 11:45:53 -0400

    devices from all users in admin panel

--- iot_devices/admin.py
+++ iot_devices/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .models import Device
 
 # Register your models here.
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ['added_by', 'type', 'name', 'device_id',]
+    list_filter = ['added_by', 'type', 'device_id',]
 
-admin.site.register(Device)
+admin.site.register(Device, DeviceAdmin)
